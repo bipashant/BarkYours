a5892238f1982ad5c18c115ecf6a2cef9651ec51
Bibek Sharma CHapagain <bipashant@gmail.com>
2018-03-28 19:49:05 +0545

    initial commit

---
+++ content.js
@@ -0,0 +1,96 @@
+
+var featuredCategories = [
+    {
+        "title": "Clothing Accessories",
+        "image": "https://www.sportslocater.com/Images/grid_icon.png",
+        "url": "https://barkyours.sharetribe.com/?category=clothing-and-accessories"
+    },
+    {
+        "title": "Clothing Accessories",
+        "image": "https://static-blog.fabfitfun.com/magazine/wp-content/uploads/2017/08/25175621/Screen-Shot-2017-08-25-at-5.55.40-PM.jpg",
+        "url": "https://barkyours.sharetribe.com/?category=clothing-and-accessories"
+    },
+    {
+        "title": "Clothing Accessories",
+        "image": "https://static-blog.fabfitfun.com/magazine/wp-content/uploads/2017/08/25175621/Screen-Shot-2017-08-25-at-5.55.40-PM.jpg",
+        "url": "https://barkyours.sharetribe.com/?category=clothing-and-accessories"
+    }
+];
+var itemsOfWeek = [
+    {
+        "title": "Tshirt",
+        "image": "https://static-blog.fabfitfun.com/magazine/wp-content/uploads/2017/08/25175621/Screen-Shot-2017-08-25-at-5.55.40-PM.jpg",
+        "url": "https://barkyours.sharetribe.com/?category=clothing-and-accessories",
+        "price": "$123.45",
+        "seller_name": "Tammi Shapiro",
+        "seller_profile_url": "https://barkyours.sharetribe.com/en/tammis"
+    },
+    {
+        "title": "Tshirt",
+        "image": "https://static-blog.fabfitfun.com/magazine/wp-content/uploads/2017/08/25175621/Screen-Shot-2017-08-25-at-5.55.40-PM.jpg",
+        "url": "https://barkyours.sharetribe.com/?category=clothing-and-accessories",
+        "price": "$123.45",
+        "seller_name": "Tammi Shapiro",
+        "seller_profile_url": "https://barkyours.sharetribe.com/en/tammis"
+    },
+    {
+        "title": "View all Listings",
+        "image": "https://bytebucket.org/bipashant/barkyours/raw/1df09ff6604980ea81e51b59cb1f6c7650da35b7/view-all.jpg",
+        "url": "https://barkyours.sharetribe.com/?view=grid",
+        "price": "$123.45",
+        "seller_name": "Tammi Shapiro",
+        "seller_profile_url": "https://barkyours.sharetribe.com/en/tammis"
+    }
+];
+
+var questionAnswer = [
+    {
+        "qns": 'I am a Bark Yours Shopper',
+        "ans": 'Answer goes here.'
+    },
+    {
+        "qns": 'I am a Bark Yours Seller',
+        "ans": 'Answer goes here'
+    },
+    {
+        "qns": 'Next Question goes here 1',
+        "ans": 'Next Answer goes here 1'
+    },
+    {
+        "qns": 'Next Question goes here 2',
+        "ans": 'Next Answer goes here 2'
+    },
+    {
+        "qns": 'Next Question goes here 3',
+        "ans": 'Next Answer goes here 3'
+    },
+    {
+        "qns": 'Next Question goes here 4',
+        "ans": 'Next Answer goes here 4'
+    },
+    {
+        "qns": 'Next Question goes here 5',
+        "ans": 'Next Answer goes here 5'
+    },
+    {
+        "qns": 'Next Question goes here 6',
+        "ans": 'Next Answer goes here 6'
+    },
+    {
+        "qns": 'Next Question goes here 7',
+        "ans": 'Next Answer goes here 7'
+    },
+    {
+        "qns": 'Next Question goes here 8',
+        "ans": 'Next Answer goes here 8'
+    },
+    {
+        "qns": 'Next Question goes here 9',
+        "ans": 'Next Answer goes here 9'
+    },
+    {
+        "qns": 'Next Question goes here 10',
+        "ans": 'Next Answer goes here 10'
+    }
+];
+
