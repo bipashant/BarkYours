7eb3a12d6917f2f94fda039174cba6dc9981626b
Bibek Sharma Chapagain <bibeksharma@lftechnology.com>
2018-03-30 01:17:00 +0545

    Update content.js

--- content.js
+++ content.js
@@ -2,34 +2,34 @@
 var featuredCategories = [
     {
         "title": "Clothing Accessories",
+        "image": "https://www.sportslocater.com/Images/grid_icon.png",
+        "url": "https://barkyours.sharetribe.com/?category=clothing-and-accessories"
+    },
+    {
+        "title": "Clothing Accessories",
         "image": "https://static-blog.fabfitfun.com/magazine/wp-content/uploads/2017/08/25175621/Screen-Shot-2017-08-25-at-5.55.40-PM.jpg",
         "url": "https://barkyours.sharetribe.com/?category=clothing-and-accessories"
     },
     {
-        "title": "Jwelry",
+        "title": "Clothing Accessories",
         "image": "https://static-blog.fabfitfun.com/magazine/wp-content/uploads/2017/08/25175621/Screen-Shot-2017-08-25-at-5.55.40-PM.jpg",
-        "url": "https://barkyours.sharetribe.com/?category=jwelry"
-    },
-    {
-        "title": "Toys and Entertainment",
-        "image": "https://static-blog.fabfitfun.com/magazine/wp-content/uploads/2017/08/25175621/Screen-Shot-2017-08-25-at-5.55.40-PM.jpg",
-        "url": "https://barkyours.sharetribe.com/?category=toys-and-entertainment"
+        "url": "https://barkyours.sharetribe.com/?category=clothing-and-accessories"
     }
 ];
 var itemsOfWeek = [
     {
-        "title": "This is very long listing title",
-        "image": "https://user-assets.sharetribe.com/images/listing_images/images/1451248/big/discodog_03.gif",
-        "url": "https://barkyours.sharetribe.com/en/listings/550616",
-        "price": "$10.00",
+        "title": "Tshirt",
+        "image": "https://static-blog.fabfitfun.com/magazine/wp-content/uploads/2017/08/25175621/Screen-Shot-2017-08-25-at-5.55.40-PM.jpg",
+        "url": "https://barkyours.sharetribe.com/?category=clothing-and-accessories",
+        "price": "$123.45",
         "seller_name": "Tammi Shapiro",
         "seller_profile_url": "https://barkyours.sharetribe.com/en/tammis"
     },
     {
-        "title": "This is very long listing title also for mobile.",
-        "image": "https://user-assets.sharetribe.com/images/listing_images/images/1451259/big/petfirstaid.jpg",
-        "url": "https://barkyours.sharetribe.com/en/listings/550189",
-        "price": "$1,999.99",
+        "title": "Tshirt",
+        "image": "https://static-blog.fabfitfun.com/magazine/wp-content/uploads/2017/08/25175621/Screen-Shot-2017-08-25-at-5.55.40-PM.jpg",
+        "url": "https://barkyours.sharetribe.com/?category=clothing-and-accessories",
+        "price": "$123.45",
         "seller_name": "Tammi Shapiro",
         "seller_profile_url": "https://barkyours.sharetribe.com/en/tammis"
     },
@@ -43,11 +43,8 @@ var itemsOfWeek = [
     }
 ];
 
-var questionAnswer = [
-    {
-        "qns": 'I am a Bark Yours Shopper',
-        "ans": 'Answer goes here.'
-    },
+var questionAnswerForSeller = [
+
     {
         "qns": 'I am a Bark Yours Seller',
         "ans": 'Answer goes here'
@@ -59,38 +56,20 @@ var questionAnswer = [
     {
         "qns": 'Next Question goes here 2',
         "ans": 'Next Answer goes here 2'
-    },
-    {
-        "qns": 'Next Question goes here 3',
-        "ans": 'Next Answer goes here 3'
-    },
-    {
-        "qns": 'Next Question goes here 4',
-        "ans": 'Next Answer goes here 4'
-    },
-    {
-        "qns": 'Next Question goes here 5',
-        "ans": 'Next Answer goes here 5'
-    },
-    {
-        "qns": 'Next Question goes here 6',
-        "ans": 'Next Answer goes here 6'
-    },
-    {
-        "qns": 'Next Question goes here 7',
-        "ans": 'Next Answer goes here 7'
-    },
-    {
-        "qns": 'Next Question goes here 8',
-        "ans": 'Next Answer goes here 8'
-    },
-    {
-        "qns": 'Next Question goes here 9',
-        "ans": 'Next Answer goes here 9'
-    },
-    {
-        "qns": 'Next Question goes here 10',
-        "ans": 'Next Answer goes here 10'
     }
 ];
 
+var questionAnswerForShopper = [
+    {
+        "qns": 'I am a Bark Yours Shopper',
+        "ans": 'Answer goes here.'
+    },
+    {
+        "qns": 'Next Question goes here 1',
+        "ans": 'Next Answer goes here 1'
+    },
+    {
+        "qns": 'Next Question goes here 2',
+        "ans": 'Next Answer goes here 2'
+    }
+];
